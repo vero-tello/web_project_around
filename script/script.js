cccfaed880d7141ee27d2a39b4688f9f7cd56ac4
Vero.Tello <veronica_iausa.zitacuaro@hotmail.com>
2024-12-12 19:44:04 -0700

    agregue main, popup y reglas css para ambas, asi como js para correcto funcionamiento de acuerdo al diseño

---
+++ script/script.js
@@ -0,0 +1,33 @@
+let profileName = document.querySelector(".profile__paragraph_name");
+let profileJob = document.querySelector(".profile__paragraph_about");
+let editButton = document.querySelector(".profile__button");
+
+let popupElement = document.querySelector(".popup");
+let popupCloseButton = popupElement.querySelector(".popup__close");
+let formElement = document.querySelector(".popup__form");
+let nameInput = formElement.querySelector(".popup__input-name");
+let jobInput = formElement.querySelector(".popup__input-job");
+
+function openPopup() {
+  popupElement.classList.add("popup_open");
+
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+function closePopup() {
+  popupElement.classList.remove("popup_open");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+
+  closePopup();
+}
+
+editButton.addEventListener("click", openPopup);
+popupCloseButton.addEventListener("click", closePopup);
+formElement.addEventListener("submit", handleProfileFormSubmit);
